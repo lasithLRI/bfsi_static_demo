5247de802ccc97b21921c30f782877c0c880a426
lasithLRI <lasith.hettiarachchi.dev@gmail.com>
2025-11-07 14:24:49 +0530

    initiate

--- src/main.tsx
+++ src/main.tsx
@@ -20,7 +20,7 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.scss'
 import App from './app.tsx'
-import {BrowserRouter} from "react-router-dom";
+import { HashRouter} from "react-router-dom";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
 export  const queryClient = new QueryClient()
@@ -33,9 +33,9 @@ export  const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
-            <BrowserRouter>
+            <HashRouter>
                 <App/>
-            </BrowserRouter>
+            </HashRouter>
         </QueryClientProvider>
     </StrictMode>,
 )
